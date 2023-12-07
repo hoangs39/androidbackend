53cb9ebab0724c152ad5f200abf02fc058fb3356
hoangs39 <s3927241@rmit.edu.vn>
2023-12-07 14:19:59 +0700

    new 2

--- index.js
+++ index.js
@@ -121,5 +121,5 @@ app.get('/printAllData', async (req, res) => {
 //     }
 // })
 
-
-app.listen(8000, () => console.log("Express Server started on port 8000"));
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log("Express Server started on port 8000"));
